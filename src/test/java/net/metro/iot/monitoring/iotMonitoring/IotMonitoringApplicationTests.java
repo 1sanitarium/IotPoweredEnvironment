51640f88f44af45b34ae50b7dd516c0992165dac
Vlad <alexandru.vlad@metrosystems.net>
2018-06-25 12:04:21 +0300

    initial commit

---
+++ src/test/java/net/metro/iot/monitoring/iotMonitoring/IotMonitoringApplicationTests.java
@@ -0,0 +1,16 @@
+package net.metro.iot.monitoring.iotMonitoring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IotMonitoringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
