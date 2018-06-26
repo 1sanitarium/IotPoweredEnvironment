f4249703ed3d21b776f36c1ce26530ebaeb57e09
Vlad <alexandru.vlad@metrosystems.net>
2018-06-26 14:48:43 +0300

    fuck yea

--- src/main/java/net/metro/iot/monitoring/iotMonitoring/service/SensorValueService.java
+++ src/main/java/net/metro/iot/monitoring/iotMonitoring/service/SensorValueService.java
@@ -3,6 +3,7 @@ package net.metro.iot.monitoring.iotMonitoring.service;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,19 +29,19 @@ public class SensorValueService {
     public void save(SensorValueDto sensorValueDto) {
         Field[] fields = SensorValueDto.class.getDeclaredFields();
         LOGGER.info(fields.toString());
-        Arrays.asList(fields).stream().filter(filter -> filter.getName().equals("devideId")).forEach(field -> {
+        Arrays.asList(fields).stream().filter(filter -> !filter.getName().equals("deviceId")).forEach(field -> {
             field.setAccessible(true);
             String value = "";
             try {
                 value = (String) field.get(sensorValueDto);
                 LOGGER.info(value);
+
+                if (Objects.nonNull(value)) {
+                    SensorValue sensorValue = new SensorValue(sensorValueDto.getDeviceId(), value, new Date(), null, field.getName());
+                    sensorValueDao.save(sensorValue);
+                }
             } catch (IllegalArgumentException | IllegalAccessException e) {
                 LOGGER.info(e.toString());
-            }
-            SensorValue sensorValue = new SensorValue(sensorValueDto.getDeviceId(), value, new Date(), null, field.getName());
-
-            if (!sensorValue.getValue().isEmpty()) {
-                sensorValueDao.save(sensorValue);
             }
 
         });
