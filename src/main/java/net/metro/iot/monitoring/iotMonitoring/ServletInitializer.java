5c8130d1fc71de2678a405a04431862fa0baf342
Vlad <alexandru.vlad@metrosystems.net>
2018-06-26 09:35:14 +0300

    changed dependencies

---
+++ src/main/java/net/metro/iot/monitoring/iotMonitoring/ServletInitializer.java
@@ -0,0 +1,13 @@
+package net.metro.iot.monitoring.iotMonitoring;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(IotMonitoringApplication.class);
+    }
+
+}
