f74ec967fe273cec83c6631bac12b97521b67562
Vlad <alexandru.vlad@metrosystems.net>
2018-06-27 09:21:21 +0300

    datadog

--- src/main/java/net/metro/iot/monitoring/iotMonitoring/controller/SensorValuesController.java
+++ src/main/java/net/metro/iot/monitoring/iotMonitoring/controller/SensorValuesController.java
@@ -3,6 +3,7 @@ package net.metro.iot.monitoring.iotMonitoring.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.metrics.instrument.MeterRegistry;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,14 +16,21 @@ import net.metro.iot.monitoring.iotMonitoring.service.SensorValueService;
 @RequestMapping("/sensor_value")
 public class SensorValuesController {
 
-    //private static final Logger LOGGER = LoggerFactory.getLogger(SensorValuesController.class);
+    private final MeterRegistry registry;
+
+    private final SensorValueService sensorValueService;
 
     @Autowired
-    private SensorValueService sensorValueService;
+    public SensorValuesController(MeterRegistry registry, SensorValueService sensorValueService) {
+        this.registry = registry;
+        this.sensorValueService = sensorValueService;
+    }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public void save(@RequestBody SensorValueDto sensorValueDto, HttpServletRequest request) throws Exception {
+
+        Counter.builder("iot.sensorConfigService.retrieveALLCounter").register(registry).increment();
+
         sensorValueService.save(sensorValueDto);
     }
-
 }
